9a256802c56b558ada2b7aa455c7b7135aa12743
Adewale Orotayo <didymus7007@gmail.com>
2020-05-04 21:20:04 +0100

    Author_session_controller's destroy action is buggy But the rest are working fine

---
+++ app/controllers/author_sessions_controller.rb
@@ -0,0 +1,31 @@
+
+
+
+class AuthorSessionsController < ApplicationController
+
+  before_filter :zero_authors_or_authenticated, only: [:new, :create]
+
+  def zero_authors_or_authenticated
+    unless Author.count == 0 || current_user
+      redirect_to root_path
+      return false
+    end
+  end
+  
+  def new
+  end
+
+  def create
+    if login(params[:email], params[:password])
+      redirect_back_or_to(articles_path, notice: 'Logged in successfully.')
+    else
+      flash.now.alert = "Login failed."
+      render action: :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:authors, notice: 'Logged out!')
+  end
+end
