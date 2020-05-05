48e87a7ffea45961d8b960c7505cf58b14c73dfe
Adewale Orotayo <didymus7007@gmail.com>
2020-05-05 14:59:53 +0100

    Authentication functional

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -1,16 +1,4 @@
-
-
-
 class AuthorSessionsController < ApplicationController
-
-  before_filter :zero_authors_or_authenticated, only: [:new, :create]
-
-  def zero_authors_or_authenticated
-    unless Author.count == 0 || current_user
-      redirect_to root_path
-      return false
-    end
-  end
   
   def new
   end
@@ -28,4 +16,6 @@ class AuthorSessionsController < ApplicationController
     logout
     redirect_to(:authors, notice: 'Logged out!')
   end
+
+
 end
